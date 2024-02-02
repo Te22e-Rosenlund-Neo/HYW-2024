fbd9c97a8a16dfc51e9e14a688f0246f15970f65
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-02-02 11:57:09 +0100

    AiUi

--- Assets/UnityStuf/Scripts/ChatBotInput.cs
+++ Assets/UnityStuf/Scripts/ChatBotInput.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
 
 public class ChatBotInput : MonoBehaviour
@@ -9,13 +10,21 @@ public class ChatBotInput : MonoBehaviour
     string UserInput;
     string Response;
     public TMP_InputField InputField;
-
+    public Canvas canvas;
+    GameObject Panel;
     void Update(){
         
     }
 
-    void Input(){
+    public void Send(){
         UserInput = InputField.text;
+        Debug.Log(UserInput);
+    }
+    public void Reset(){
+        InputField.text = "";
+    }
+    public void disable(){
+        canvas.enabled = false;
     }
  
 }
