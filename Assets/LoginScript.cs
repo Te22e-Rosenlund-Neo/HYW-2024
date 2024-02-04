b69cbabab160816f27aa1a07f20cc915b58f9755
Virtknight <Neo.rosenlund@elev.ga.ntig.se>
2024-02-04 12:30:05 +0100

    added quick comments

--- Assets/LoginScript.cs
+++ Assets/LoginScript.cs
@@ -11,6 +11,7 @@ public class LoginScript : MonoBehaviour
     string UsernameInput;
     string passwordInput;
 
+//takes user input when interacted with. decides if it should create an account of login
     public void TakeInput(bool Create){
         UsernameInput = UsernameInputfield.text;
         passwordInput = PasswordInputfield.text;
@@ -20,14 +21,17 @@ public class LoginScript : MonoBehaviour
             Login(UsernameInput, passwordInput);
         }
     }
+    //here we set the password to be a value of the key username. 
     void CreateAcount(string UserName, string PassWord){
 
         PlayerPrefs.SetString(UserName, PassWord);
         // PlayerPrefs.DeleteAll();
     }
-    void Login(string UserName, string PassWord){
 
-        if(PlayerPrefs.GetString(UserName) == PassWord){
+    //we check if the usernames(key) value is the same as the password that was put in
+    void Login(string AccUserName, string InputPassWord){
+
+        if(PlayerPrefs.GetString(AccUserName) == InputPassWord){
             Debug.Log("Logged in!");
         }else{
             Debug.Log("Wrong Password or username");
