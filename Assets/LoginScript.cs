def7e90ea6f2d949b4f7fe8e515dc2b2a952f606
Virtknight <Neo.rosenlund@elev.ga.ntig.se>
2024-02-04 12:26:37 +0100

    Added a save acoutn system using playerprefs

--- Assets/LoginScript.cs
+++ Assets/LoginScript.cs
@@ -8,20 +8,30 @@ public class LoginScript : MonoBehaviour
     public TMP_InputField UsernameInputfield;
     public TMP_InputField PasswordInputfield;
 
-    string CorrectName = "name";
-    string CorrectPassword = "password";
-
     string UsernameInput;
     string passwordInput;
 
-    public void TakeInput(){
+    public void TakeInput(bool Create){
         UsernameInput = UsernameInputfield.text;
         passwordInput = PasswordInputfield.text;
-        CheckInput(UsernameInput,passwordInput);
-    }
-    public void CheckInput(string Username, string password){
-        if(Username == CorrectName && password == CorrectPassword){
-            Debug.Log("correct!");
+        if(Create == true){
+            CreateAcount(UsernameInput, passwordInput);
+        }else{
+            Login(UsernameInput, passwordInput);
         }
+    }
+    void CreateAcount(string UserName, string PassWord){
+
+        PlayerPrefs.SetString(UserName, PassWord);
+        // PlayerPrefs.DeleteAll();
+    }
+    void Login(string UserName, string PassWord){
+
+        if(PlayerPrefs.GetString(UserName) == PassWord){
+            Debug.Log("Logged in!");
+        }else{
+            Debug.Log("Wrong Password or username");
+        }
+
     }
 }
